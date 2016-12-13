ce7c088028cc5bbb0834c884b8033300182770dc
Dale Race <dale.race@daugherty.com>
2016-12-13 11:18:35 -0600

    Clean up inputs, remove padding around profile page boxes

--- gamerTAG/src/components/profile/tagInfo.js
+++ gamerTAG/src/components/profile/tagInfo.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import {Col} from 'react-bootstrap';
+import TextField from 'material-ui/TextField';
+import {orange800} from 'material-ui/styles/colors';
+
 
 var TagInfo = React.createClass({
     render: function() {
+      var styles = {
+        underlineStyle: {
+          borderColor: orange800,
+        },
+      };
         return (
             <div className="tagSection">
                 <Col xs={12}>
@@ -11,25 +19,37 @@ var TagInfo = React.createClass({
                         <Col sm={12} md={6}>
                             <div>
                                 <p>LoL Summoner Name</p>
-                                <input type="text"/>
+                                <TextField
+                                  className="gamertag_input"
+                                  underlineFocusStyle={styles.underlineStyle}
+                                  hintText="LoL Summoner Name" />
                             </div>
                         </Col>
                         <Col sm={12} md={6}>
                             <div>
                                 <p>Blizzard BattleTag</p>
-                                <input type="text"/>
+                                <TextField
+                                  className="gamertag_input"
+                                  underlineFocusStyle={styles.underlineStyle}
+                                  hintText="Blizzard BattleTag" />
                             </div>
                         </Col>
                         <Col sm={12} md={6}>
                             <div>
                                 <p>Xbox Gamertag</p>
-                                <input type="text"/>
+                                <TextField
+                                  className="gamertag_input"
+                                  underlineFocusStyle={styles.underlineStyle}
+                                  hintText="Xbox Gamertag" />
                             </div>
                         </Col>
                         <Col sm={12} md={6}>
                             <div>
                                 <p>Playstation UserName</p>
-                                <input type="text"/>
+                                <TextField
+                                  className="gamertag_input"
+                                  underlineFocusStyle={styles.underlineStyle}
+                                  hintText="Playstation UserName" />
                             </div>
                         </Col>
                     </div>
