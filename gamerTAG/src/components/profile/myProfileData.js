c19d62dfefba9bee3acf092f72b0b1030a982614
Dale Race <dale.race@daugherty.com>
2016-12-13 14:24:20 -0600

    Clean up profile data a little

--- gamerTAG/src/components/profile/myProfileData.js
+++ gamerTAG/src/components/profile/myProfileData.js
@@ -1,37 +1,33 @@
 import React from 'react';
 import {Row, Col} from 'react-bootstrap';
+import './myProfileData.css';
 
 var MyProfileData = React.createClass({
     render: function() {
         return (
-            <div>
-                <Col xs={12} className="myProfileData">
-                    <h2>Profile</h2>
-                    <Col xs={12} sm={4}>
-                        <div className="profilePic">
-                            <img src='/images/default_avatar.jpg' alt="profile-pic"/>
-                        </div>
-                    </Col>
-                    <Col xs={12} sm={4}>
-                        <div className="userInfo">
-                            <p className="tag">gamerTAG: {this.props.gamerTAG}</p>
-                            <p>Name: {this.props.name}</p>
-                            <p>Location: {this.props.location}</p>
-                            <p>Status: {this.props.status}</p>
-
-                        </div>
-                    </Col>
-                    <Col xs={12} sm={4}>
-                        <Col xs={12} sm={12}>
-                            <p>Current Game:</p>
-                        </Col>
-                        <Col xs={12} sm={12}>
-                            <p>{this.props.recentGame}</p>
-                        </Col>
-
-                    </Col>
-                </Col>
-            </div>
+            <Row className="myProfileData">
+              <h2>Profile</h2>
+              <Col xs={12} sm={4}>
+                <div className="profilePic">
+                  <img src='/images/default_avatar.jpg' alt="profile-pic"/>
+                </div>
+              </Col>
+              <Col xs={12} sm={4}>
+                <div className="userInfo">
+                  <p className="tag"><strong>gamerTAG</strong>: {this.props.gamerTAG}</p>
+                  <p className="name"><strong>Name</strong>: {this.props.name}</p>
+                  <p className="location"><strong>Location</strong>: {this.props.location}</p>
+                  <p className="status"><strong>Status</strong>: {this.props.status}</p>
+                </div>
+              </Col>
+              <Col xs={12} sm={4}>
+                <Row>
+                  <Col xs={12} sm={12}>
+                    <p className="current-game"><strong>Current Game</strong>: {this.props.recentGame}</p>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
         )
     }
 });
