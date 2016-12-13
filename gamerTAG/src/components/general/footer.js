825c0ec2963f72dadd8b09ea7b04a08a78ef2104
CORPORATE\NPT0726 <nic.tolmais@daugherty.com>
2016-12-13 13:10:48 -0600

    Changed footer

--- gamerTAG/src/components/general/footer.js
+++ gamerTAG/src/components/general/footer.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './footer.css';
+import {Row, Col} from 'react-bootstrap';
 
 var Footer = React.createClass({
     render: function() {
         return (
           <div className="footer">
-              This is the footer
+            <Col xs={12} sm={4}>
+              <p>Getting Started</p>
+              <p>About Us</p>
+              <p>Contact Us</p>
+            </Col>
+            <Col xs={12} sm={4}>
+              <div>
+                <img src={require('../../img/gamerTAG.png')} alt="profile-pic" />
+              </div>
+            </Col>
+            <Col xs={12} sm={4}>
+              <p>©MetaDev Technologies</p>
+              <p>Privacy</p>
+              <p>Terms and Usage</p>
+            </Col>
           </div>
         )
     }
