7a3c0fcf258de6d0e73d77c91bf090da2286bc21
Dale Race <dale.race@daugherty.com>
2016-12-14 10:39:18 -0600

    Copy code from Landing page

--- gamerTAG/src/components/landing/landing.js
+++ gamerTAG/src/components/landing/landing.js
@@ -1,39 +1,66 @@
 import React from 'react';
-import {Row, Col} from 'react-bootstrap';
+import {Grid, Row, Col} from 'react-bootstrap';
 import { Link } from 'react-router';
+import RaisedButton from 'material-ui/RaisedButton';
+import PersonAdd from 'material-ui/svg-icons/social/person-add';
+import EnterApp from 'material-ui/svg-icons/action/exit-to-app';
+import { orange400 } from 'material-ui/styles/colors';
 import './landing.css';
 
+var styles = {
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    padding: '5% 0'
+  },
+  signUpButton: {
+    height: '200px',
+    lineHeight: '200px',
+    width: '300px',
+    border: '1px solid #555'
+  },
+  signInButton: {
+    height: '200px',
+    lineHeight: '200px',
+    width: '300px',
+    border: '1px solid #555'
+  }
+}
 var Landing = React.createClass({
     render: function() {
         return (
-            <div className="account-creation all-page-layout">
+            <Grid className="account-creation">
                 <form className="account-information">
                     <Row className="show-grid">
-                        <Col xs={8} xsOffset={2}>
-                            <div className="app-description">
-                                <p>
-                                  GAMERTAG is the way to link gamers who what to share their experience of playing awesome video games they are currently into.
-                                  Whether you want to casual or go balls to the walls, we will find other gamers with a similar skill level and game preferences as you.
-                                  
-                                </p>
-                            </div>
-
-                        </Col>
+                      <Col xs={12}>
+                        <div className="app-description">
+                          <h1>Welcome to GamerTAG</h1>
+                          <p>GAMERTAG is the way to link gamers who what to share their experience of playing awesome video games they are currently into. Whether you want to casual or go balls to the walls, we will find other gamers with a similar skill level and game preferences as you.</p>
+                        </div>
+                      </Col>
                     </Row>
-                    <Row className="show-grid">
-                        <Col xs={6}>
-                            <div className="sign-up-button">
-                                <Link to='/create-account'><button type="button" className="btn btn-default">Sign Up</button></Link>
-                            </div>
-                        </Col>
-                        <Col xs={6}>
-                            <div className="sign-in-button">
-                                <Link to='/sign-in'><button type="button" className="btn btn-default">Sign In</button></Link>
-                            </div>
-                        </Col>
-                    </Row>
+                    <div style={styles.buttonContainer}>
+                        <Link to='/create-account'>
+                        <RaisedButton
+                          style={styles.signUpButton}
+                          backgroundColor={orange400}
+                          label="Sign Up"
+                          icon={<PersonAdd />}
+                          />
+                        </Link>
+                        <Link to='/sign-in'>
+                        <RaisedButton
+                          style={styles.signInButton}
+                          backgroundColor={orange400}
+                          label="Sign In"
+                          icon={<EnterApp />}
+                          />
+                        </Link>
+                    </div>
                 </form>
-            </div>
+            </Grid>
         )
     }
 });
