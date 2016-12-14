fb51c7ac8191328db71386d938f4dad28b73ecbd
Dale Race <dale.race@daugherty.com>
2016-12-14 09:58:50 -0600

    Add Grid and Row to footer

--- gamerTAG/src/components/general/footer.js
+++ gamerTAG/src/components/general/footer.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import './footer.css';
-import {Row, Col} from 'react-bootstrap';
+import {Grid, Row, Col} from 'react-bootstrap';
 
 var Footer = React.createClass({
     render: function() {
         return (
-          <div className="footer">
-            <Col xs={12} sm={4}>
-              <p>Getting Started</p>
-              <p>About Us</p>
-              <p>Contact Us</p>
-            </Col>
-            <Col xs={12} sm={4}>
-              <div>
-                <img src='/images/gamerTAG.png' alt="logo-pic" />
-              </div>
-            </Col>
-            <Col xs={12} sm={4}>
-              <p>©MetaDev Technologies</p>
-              <p>Privacy</p>
-              <p>Terms and Usage</p>
-            </Col>
-          </div>
+          <Grid className="footer" fluid>
+            <Row>
+              <Col xs={12} sm={4}>
+                <p>Getting Started</p>
+                <p>About Us</p>
+                <p>Contact Us</p>
+              </Col>
+              <Col xs={12} sm={4}>
+                <div>
+                  <img src='/images/gamerTAG.png' alt="logo-pic" />
+                </div>
+              </Col>
+              <Col xs={12} sm={4}>
+                <p>©MetaDev Technologies</p>
+                <p>Privacy</p>
+                <p>Terms and Usage</p>
+              </Col>
+            </Row>
+          </Grid>
         )
     }
 });
