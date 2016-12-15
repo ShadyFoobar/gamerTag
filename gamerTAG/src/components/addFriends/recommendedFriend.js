a437e54316931bf834a5b0f3e3bdfe04628f8ac7
Dale Race <dale.race@daugherty.com>
2016-12-15 15:15:45 -0600

    Make recommendedFriend backgrounds transparent, and fixed + buttons to top right

--- gamerTAG/src/components/addFriends/recommendedFriend.js
+++ gamerTAG/src/components/addFriends/recommendedFriend.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import {Row, Col} from 'react-bootstrap';
-import Paper from 'material-ui/Paper';
 import './recommendedFriend.css';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import {blueGrey500} from 'material-ui/styles/colors';
+
 
 var RecommendedFriend = React.createClass({
     render: function() {
         return (
             <div>
-                <Paper zDepth={3} className="reccomendedFriendPaper">
-                    <Row className="show-grid reccomendedFriend">
+              <Row className="show-grid reccomendedFriend">
                         <Col xs={12}>
                             <div className="friendData">
                                 <Row>
-                                    <Col xs={10} id="reccomended-friend-name">
+                                    <Col xs={9} md={10} id="reccomended-friend-name">
                                         <p>{this.props.friendName}</p>
                                     </Col>
-                                    <Col xs={2}>
-                                        <FloatingActionButton mini={true} className="addFriendButton">
+                                    <Col xs={3} md={2}>
+                                        <FloatingActionButton
+                                          className="addFriendButton"
+                                          mini={true}
+                                          backgroundColor={blueGrey500}
+                                        >
                                             <ContentAdd/>
                                         </FloatingActionButton>
                                     </Col>
@@ -37,7 +41,6 @@ var RecommendedFriend = React.createClass({
                             </div>
                         </Col>
                     </Row>
-                </Paper>
             </div>
         )
     }
