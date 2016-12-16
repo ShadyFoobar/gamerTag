c0ea2ed2ce1952d8f30e3ee794d4e7e4bc9f16e7
CORPORATE\NPT0726 <nic.tolmais@daugherty.com>
2016-12-16 13:34:55 -0600

    started redo of profile page. currently bare with one section white and few data

--- gamerTAG/src/components/profile/profile-page.js
+++ gamerTAG/src/components/profile/profile-page.js
@@ -5,6 +5,9 @@ import GamerTagInfo from './gamerTagInfo/gamerTagInfo';
 import AddGame from './addGame/addGame';
 import {Row, Col, Grid} from 'react-bootstrap';
 import data from '../../data.js';
+import {Card} from 'material-ui/Card';
+import {orange600} from 'material-ui/styles/colors';
+import {CardMedia, CardTitle} from 'material-ui/Card';
 import './profile-page.css';
 
 var ProfilePage = React.createClass({
@@ -15,29 +18,86 @@ var ProfilePage = React.createClass({
         this.props.changeLogIn();
     },
     render: function() {
+        const styles = {
+            customWidth: {
+                width: 150
+            },
+            card: {
+                maxWidth: '10000px',
+                margin: '0 auto',
+                padding: '10px 0',
+                backgroundColor: '#eee'
+            },
+            floatingLabel: {
+                color: orange600
+            }
+        };
         return (
-            <div>
-                <Grid className="all-page-layout">
-                    <Row className="show-grid profilePageSplit">
-                        <Col xs={12}>
-                            <MyProfileData gamerTAG={this.state.user.gamerTAG} name={this.state.user.name} location={this.state.user.location} status={this.state.user.status} recentGame={this.state.user.recentGame}/>
-                        </Col>
-                    </Row>
-                    <Row className="show-grid profilePageSplit">
-                        <Col xs={12} sm={6}>
-                            <AddGame/>
-                        </Col>
-                        <Col xs={12} sm={6}>
-                            <GamerTagInfo/>
-                        </Col>
-                    </Row>
-                    <Row className="show-grid profilePageSplit bottomPageSplit">
-                        <Col xs={12}>
-                            <AllGameStats/>
-                        </Col>
-                    </Row>
+                <Grid>
+                    <Card style={styles.card}>
+                        <Row className="show-grid">
+                            <Col xs={12} className="profilePageHeader">
+                                <h1>- My Profile -</h1>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={12} sm={4}>
+                                <div className="profilePic">
+                                  <CardMedia
+                                    overlay={<CardTitle
+                                      title={this.props.gamerTAG}
+                                      subtitle="Pro"
+                                      style={styles.cardTitle} />}
+                                   >
+                                    <img src='/images/default_avatar.jpg' alt="profile-pic"/>
+                                  </CardMedia>
+                                </div>
+                            </Col>
+                            <Col xs={12} sm={4}>
+                                <div>
+                                    <p>
+                                        <strong>Name</strong>: {this.state.user.name}
+                                    </p>
+                                    <p>
+                                        <strong>Location</strong>: {this.state.user.location}
+                                    </p>
+                                    <p>
+                                        <strong>Status</strong>: {this.state.user.status}
+                                    </p>
+                                </div>
+                            </Col>
+                            <Col xs={12} sm={4}>
+                                <Row>
+                                    <Col sm={12}>
+                                        <div>
+                                            <p><strong>LoL Summoner Name</strong></p>
+                                        </div>
+                                    </Col>
+                                    <Col sm={12}>
+                                        <div>
+                                            <p><strong>Blizzard BattleTag</strong></p>
+                                        </div>
+                                    </Col>
+                                    <Col sm={12}>
+                                        <div>
+                                            <p><strong>Xbox Gamertag</strong></p>
+                                        </div>
+                                    </Col>
+                                    <Col sm={12}>
+                                        <div>
+                                            <p><strong>Playstation UserName</strong></p>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                        <Row className="show-grid">
+                            <Col xs={12}>
+                                <AllGameStats/>
+                            </Col>
+                        </Row>
+                    </Card>
                 </Grid>
-            </div>
         )
     }
 });
